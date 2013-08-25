cec2759b980481b0e711172d07360a9df6702fa6
zhou han <han.zhou@gu3.co.jp>
2013-08-26 00:02:49 +0900

    start to creat logo layer

---
+++ Air_Hockey/Classes/LogoLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  LogoLayer.cpp
+//  Air_Hockey
+//
+//  Created by hanks on 2013/08/25.
+//
+//
+
+#include "LogoLayer.h"
---
+++ Air_Hockey/Classes/LogoLayer.h
@@ -0,0 +1,14 @@
+//
+//  LogoLayer.h
+//  Air_Hockey
+//
+//  Created by hanks on 2013/08/25.
+//
+//
+
+#ifndef __Air_Hockey__LogoLayer__
+#define __Air_Hockey__LogoLayer__
+
+#include <iostream>
+
+#endif /* defined(__Air_Hockey__LogoLayer__) */
