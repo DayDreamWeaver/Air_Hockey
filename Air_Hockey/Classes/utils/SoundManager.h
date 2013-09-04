51edcd165f272a222d278324a626fc8a9f23ef9c
zhou han <han.zhou@gu3.co.jp>
2013-09-04 22:30:13 +0900

    add SoundManager

---
+++ Air_Hockey/Classes/utils/SoundManager.h
@@ -0,0 +1,14 @@
+//
+//  SoundManager.h
+//  Air_Hockey
+//
+//  Created by hanks on 2013/09/04.
+//
+//
+
+#ifndef __Air_Hockey__SoundManager__
+#define __Air_Hockey__SoundManager__
+
+#include <iostream>
+
+#endif /* defined(__Air_Hockey__SoundManager__) */
